4065c7596f517bbeb02ec5ad57436a3234a55fc9
Dmitriy-Tyukh <roost1982@bigmir.net>
2023-01-15 17:36:28 +0200

    change class App from hooks

---
+++ src/components/App/App.jsx
@@ -0,0 +1,66 @@
+import React, { useState, useEffect } from 'react';
+import { nanoid } from 'nanoid';
+import { Box } from './App.styled'
+import defaultContacts from 'dataJson/contacts.json';
+import ContactsList from 'components/ContactsList';
+import ContactForm from 'components/ContactForm';
+import Filter from 'components/Filter';
+
+const App = () => {
+
+    const [contacts, setContacts] = useState(defaultContacts);
+    const [filter, setFilter] = useState('')
+
+    useEffect(() => {
+        const localContacts = localStorage.getItem('contacts');
+        const parseContacts = JSON.parse(localContacts);
+        if (parseContacts){setContacts(parseContacts)}
+    },[]) 
+    
+    useEffect(() => {
+        localStorage.setItem('contacts', JSON.stringify(contacts));
+    }, [contacts]) 
+
+    const handleFiltContacts = event => {
+        setFilter(event.currentTarget.value);
+  };
+
+    const filteredContacts = () => {
+        return contacts.filter(({ name }) =>
+            name.toLowerCase().includes(filter.toLowerCase())
+        )
+    };
+
+    const handleDeleteContact = dataId => {
+        return setContacts(contacts.filter(({ id }) => dataId !== id)) 
+    };
+
+    const handleAddContact = ({ name, number }) => {
+        const nameContact = name;
+        const newContact = {
+        id: nanoid(),
+        name: name,
+        number: number,
+        };
+
+        if (contacts.some(({ name }) => name === nameContact)) {
+            alert(`${nameContact} is already in contacts.`);
+            return;
+        }
+
+        setContacts(prevContacts => [...prevContacts, newContact])
+    };
+  
+    return (
+      <Box>
+        <h1>Phonebook </h1>
+        <ContactForm onSubmitForm={handleAddContact} />
+
+        <h2>Contacts</h2>
+        <Filter fiter={filter} onFiltContacts={handleFiltContacts} />
+        <ContactsList contacts={filteredContacts()} onDeleteContact={handleDeleteContact} />
+      </Box>
+    )
+  }
+
+export default App;
